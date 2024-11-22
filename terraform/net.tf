5a2a96b7c25f6650f19db109d9d1ea75fa2fc9e0
Michael Bychkowski <mbychkowski@google.com>
2024-11-22 12:50:13 -0500

    Fix TF for init deployment (#2)

--- terraform/net.tf
+++ terraform/net.tf
@@ -22,7 +22,7 @@ module "vpc" {
   subnets = [
     {
       subnet_name           = "sn-${var.customer_id}-usc1"
-      subnet_ip             = "10.11.16.0/20"
+      subnet_ip             = "10.11.0.0/20"
       subnet_region         = "us-central1"
       subnet_private_access = "true"
       subnet_flow_logs      = "false"
@@ -31,7 +31,7 @@ module "vpc" {
     },
     {
       subnet_name           = "sn-${var.customer_id}-usw1"
-      subnet_ip             = "10.12.32.0/20"
+      subnet_ip             = "10.12.0.0/20"
       subnet_region         = "us-west1"
       subnet_private_access = "true"
       subnet_flow_logs      = "false"
@@ -40,7 +40,7 @@ module "vpc" {
     },
     {
       subnet_name           = "sn-${var.customer_id}-euw1"
-      subnet_ip             = "10.21.16.0/20"
+      subnet_ip             = "10.21.0.0/20"
       subnet_region         = "europe-west1"
       subnet_private_access = "true"
       subnet_flow_logs      = "false"
@@ -49,7 +49,7 @@ module "vpc" {
     },
     {
       subnet_name           = "sn-${var.customer_id}-ase1"
-      subnet_ip             = "10.31.16.0/20"
+      subnet_ip             = "10.31.0.0/20"
       subnet_region         = "asia-southeast1"
       subnet_private_access = "true"
       subnet_flow_logs      = "false"
@@ -62,62 +62,62 @@ module "vpc" {
     "sn-${var.customer_id}-usc1" = [
       {
         range_name    = "sn-${var.customer_id}-usc1-pods1"
-        ip_cidr_range = "172.16.0.0/16"
+        ip_cidr_range = "10.111.0.0/16"
       },
       {
         range_name    = "sn-${var.customer_id}-usc1-svcs1"
-        ip_cidr_range = "192.168.0.0/26"
+        ip_cidr_range = "10.11.16.0/26"
       },
     ],
     "sn-${var.customer_id}-usw1" = [
       {
         range_name    = "sn-${var.customer_id}-usw1-pods1"
-        ip_cidr_range = "172.16.0.0/16"
+        ip_cidr_range = "10.112.0.0/16"
       },
       {
         range_name    = "sn-${var.customer_id}-usw1-svcs1"
-        ip_cidr_range = "192.168.0.0/26"
+        ip_cidr_range = "10.12.16.0/26"
       },
     ],
     "sn-${var.customer_id}-euw1" = [
       {
         range_name    = "sn-${var.customer_id}-euw1-pods1"
-        ip_cidr_range = "172.16.0.0/16"
+        ip_cidr_range = "10.121.0.0/16"
       },
       {
         range_name    = "sn-${var.customer_id}-euw1-svcs1"
-        ip_cidr_range = "192.168.0.0/26"
+        ip_cidr_range = "10.21.16.0/26"
       },
     ],
     "sn-${var.customer_id}-ase1" = [
       {
         range_name    = "sn-${var.customer_id}-ase1-pods1"
-        ip_cidr_range = "172.16.0.0/16"
+        ip_cidr_range = "10.131.0.0/16"
       },
       {
         range_name    = "sn-${var.customer_id}-ase1-svcs1"
-        ip_cidr_range = "192.168.0.0/26"
+        ip_cidr_range = "10.31.16.0/26"
       },
     ]
   }
 }
 
 resource "google_compute_router" "router" {
-  for_each   = toset(values(module.vpc.subnets_regions))
+  for_each   = toset(module.vpc.subnets_regions)
   project    = local.project.id
-  name       = "nat-router-${var.customer_id}-${each.value}"
+  name       = "nat-router-${var.customer_id}-${each.key}"
   network    = module.vpc.network_name
-  region     = each.value
+  region     = each.key
 
   depends_on = [ module.vpc ]
 }
 
 module "cloud-nat" {
-  for_each                           = toset(values(module.vpc.subnets_regions))
+  for_each                           = toset(module.vpc.subnets_regions)
   source                             = "terraform-google-modules/cloud-nat/google"
   project_id                         = local.project.id
-  region                             = each.value
-  router                             = "nat-router-${var.customer_id}-${each.value}"
-  name                               = "nat-${var.customer_id}-${each.value}"
+  region                             = each.key
+  router                             = "nat-router-${var.customer_id}-${each.key}"
+  name                               = "nat-${var.customer_id}-${each.key}"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
