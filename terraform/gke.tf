5c15430a4404d0ed6c71513aad01f069e16feca3
Michael Bychkowski <mbychkowski@google.com>
2024-11-22 11:41:14 -0500

    Initial boiler plate setup

---
+++ terraform/gke.tf
@@ -0,0 +1,78 @@
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  deletion_protection        = false
+  source                     = "terraform-google-modules/kubernetes-engine/google//modules/beta-autopilot-private-cluster"
+  version                    = "32.0.0"
+  project_id                 = local.project.id
+  name                       = "gke-${var.customer_id}"
+  region                     = var.region
+  network                    = module.vpc.network_name
+  subnetwork                 = "sn-${var.customer_id}-usc1"
+  ip_range_pods              = "sn-${var.customer_id}-usc1-pods1"
+  ip_range_services          = "sn-${var.customer_id}-usc1-svcs1"
+  horizontal_pod_autoscaling = true
+  release_channel            = "RAPID" # RAPID was chosen for L4 support.
+  kubernetes_version         = "1.29"  # We need the tip of 1.28 or 1.29 (not just default)
+  service_account            = module.sa_gke_cluster.email
+  # Google Cloud Storage (GCS) Fuse
+  gcs_fuse_csi_driver        = false
+  # enable_private_endpoint    = true
+  enable_private_nodes       = true
+  # master_ipv4_cidr_block     = "10.0.0.0/28"
+  # master_authorized_networks = [{ cidr_block = "${var.subnet}", display_name = "internal" }]
+  master_authorized_networks = [{ cidr_block = "0.0.0.0/0", display_name = "all" }]
+  # Need to allow 48 hour window in rolling 32 days For `maintenance_start_time`
+  # & `end_time` only the specified time of the day is used, the specified date
+  # is ignored (https://cloud.google.com/composer/docs/specify-maintenance-windows#terraform)
+  maintenance_recurrence = "FREQ=WEEKLY;BYDAY=SU"
+  maintenance_start_time = "2023-01-02T07:00:00Z"
+  maintenance_end_time   = "2023-01-02T19:00:00Z"
+
+  depends_on = [
+    google_service_account.sa_gke_cluster,
+    module.vpc,
+    module.cloud-nat
+  ]
+}
+
+###############################
+##### 1) SERVICE ACCOUNTS #####
+###############################
+
+# Create a service account for GKE cluster
+module "sa_gke_cluster" {
+  source       = "terraform-google-modules/service-accounts/google"
+  project_id   = local.project.id
+  names        = ["sa-${var.customer_id}-gke-cluster"]
+  display_name = "TF - GKE cluster SA"
+  project_roles = [
+    "${local.project.id}=>roles/artifactregistry.reader",
+    "${local.project.id}=>roles/cloudtrace.agent",
+    "${local.project.id}=>roles/container.developer",
+    "${local.project.id}=>roles/container.nodeServiceAgent",
+    "${local.project.id}=>roles/logging.logWriter",
+    "${local.project.id}=>roles/monitoring.metricWriter",
+    "${local.project.id}=>roles/monitoring.viewer",
+    "${local.project.id}=>roles/stackdriver.resourceMetadata.writer"
+  ]
+}
+
+# GKE Workload Identity
+# resource "google_service_account_iam_binding" "sa_gke_cluster_wi_binding" {
+#   service_account_id = google_service_account.sa_gke_cluster.name
+#   role               = "roles/iam.workloadIdentityUser"
+#   members = [
+#     "serviceAccount:${local.project.id}.svc.id.goog[${var.job_namespace}/k8s-sa-cluster]",
+#   ]
+#   depends_on = [
+#     module.gke
+#   ]
+# }
---
+++ terraform/net.tf
@@ -0,0 +1,123 @@
+# Copyright 2023 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+
+  project_id   = local.project.id
+  network_name = "vpc-${var.customer_id}"
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name           = "sn-${var.customer_id}-usc1"
+      subnet_ip             = "10.11.16.0/20"
+      subnet_region         = "us-central1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "false"
+      description           = "TF - Subnet for us-central1"
+      subnet_private_access = true
+    },
+    {
+      subnet_name           = "sn-${var.customer_id}-usw1"
+      subnet_ip             = "10.12.32.0/20"
+      subnet_region         = "us-west1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "false"
+      description           = "TF - Subnet for us-west1"
+      subnet_private_access = true
+    },
+    {
+      subnet_name           = "sn-${var.customer_id}-euw1"
+      subnet_ip             = "10.21.16.0/20"
+      subnet_region         = "europe-west1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "false"
+      description           = "TF - Subnet for europe-west1"
+      subnet_private_access = true
+    },
+    {
+      subnet_name           = "sn-${var.customer_id}-ase1"
+      subnet_ip             = "10.31.16.0/20"
+      subnet_region         = "asia-southeast1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "false"
+      description           = "TF - Subnet for asia-southeast1"
+      subnet_private_access = true
+    }
+  ]
+
+  secondary_ranges = {
+    "sn-${var.customer_id}-usc1" = [
+      {
+        range_name    = "sn-${var.customer_id}-usc1-pods1"
+        ip_cidr_range = "172.16.0.0/16"
+      },
+      {
+        range_name    = "sn-${var.customer_id}-usc1-svcs1"
+        ip_cidr_range = "192.168.0.0/26"
+      },
+    ],
+    "sn-${var.customer_id}-usw1" = [
+      {
+        range_name    = "sn-${var.customer_id}-usw1-pods1"
+        ip_cidr_range = "172.16.0.0/16"
+      },
+      {
+        range_name    = "sn-${var.customer_id}-usw1-svcs1"
+        ip_cidr_range = "192.168.0.0/26"
+      },
+    ],
+    "sn-${var.customer_id}-euw1" = [
+      {
+        range_name    = "sn-${var.customer_id}-euw1-pods1"
+        ip_cidr_range = "172.16.0.0/16"
+      },
+      {
+        range_name    = "sn-${var.customer_id}-euw1-svcs1"
+        ip_cidr_range = "192.168.0.0/26"
+      },
+    ],
+    "sn-${var.customer_id}-ase1" = [
+      {
+        range_name    = "sn-${var.customer_id}-ase1-pods1"
+        ip_cidr_range = "172.16.0.0/16"
+      },
+      {
+        range_name    = "sn-${var.customer_id}-ase1-svcs1"
+        ip_cidr_range = "192.168.0.0/26"
+      },
+    ]
+  }
+}
+
+resource "google_compute_router" "router" {
+  for_each   = toset(values(module.vpc.subnets_regions))
+  project    = local.project.id
+  name       = "nat-router-${var.customer_id}-${each.value}"
+  network    = module.vpc.network_name
+  region     = each.value
+
+  depends_on = [ module.vpc ]
+}
+
+module "cloud-nat" {
+  for_each                           = toset(values(module.vpc.subnets_regions))
+  source                             = "terraform-google-modules/cloud-nat/google"
+  project_id                         = local.project.id
+  region                             = each.value
+  router                             = "nat-router-${var.customer_id}-${each.value}"
+  name                               = "nat-${var.customer_id}-${each.value}"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
